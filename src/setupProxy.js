6639c302fdcdb8feb0efe30f0bbc2a562ee3dd2b
jaganraj1589 <37890233+jaganraj1589@users.noreply.github.com>
2020-01-27 10:17:12 +0530

    proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,34 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(
-    '/api',
-    proxy({
-      target: 'https://abu-dhabi-moments-staging.eu-staging.kacdn.net/',
-      changeOrigin: true
-    })
-  );
+  const target = 'https://abu-dhabi-moments-staging.eu-staging.kacdn.net/';
+
+app.use(
+  '/api',
+  proxy({
+    target,
+    logLevel: 'debug',
+    secure: false,
+    changeOrigin: true,
+    ws: true,
+    xfwd: true,
+    context: (_, req) =>
+      req.method !== 'GET' ||
+      (req.headers.accept && req.headers.accept.indexOf('text/html') === -1),
+    onProxyReq: req => {
+      if (req.getHeader('origin')) {
+        req.setHeader('origin', target);
+      }
+    },
+    onError: (err, req, res) => {
+      const host = req.headers && req.headers.host;
+      if (res.writeHead && !res.headersSent) {
+        res.writeHead(500);
+      }
+      res.end(`Proxy error: Could not proxy request ${req.url} from ${host} to ${target} (${err.code}).`);
+    },
+  })
+);
 };
+
+// Settings from create-react-app
